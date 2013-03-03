91f708e3ba104a596e86a738ad560f7cad7c0a38
Justin <phreenet@gmail.com>
2013-03-03 14:04:57 -0500

    Create Customer_T table and CustomerID Sequence

--- CMIS320/Project 2/justinsmithproject2step1.sql
+++ CMIS320/Project 2/justinsmithproject2step1.sql
@@ -2,13 +2,26 @@
 -- Justin Smith
 -- CMIS 320
 
+-- Create CustomerID Sequence
+-- To use SeqCustomerID.nextval
+CREATE SEQUENCE SeqCustomerID START WITH 1000 INCREMENT BY 1;
+
 -- Create all tables
 
 /*
  * Customer Table:
  * Customer_T
- * Primary Key: CustomerID
+ * Primary Key: CustomerID, use Sequence SeqCustomerID
  */
 
 CREATE TABLE Customer_T
-    (CustomerID)
+    (CustomerID         NUMBER(*,0)     NOT NULL,
+     CustomerLName      VARCHAR2(20)    NOT NULL,
+     CustomerFName      VARCHAR2(20)    NOT NULL,
+     CustomerSAddress   VARCHAR2(30)    NOT NULL,
+     CustomerCity       VARCHAR2(20)    NOT NULL,
+     CustomerState      CHAR(2)         NOT NULL,
+     CustomerZIP        VARCHAR2(5)     NOT NULL,
+     CustomerPhone      VARCHAR(10)     NOT NULL,
+
+CONSTRAINT CustomerID_PK PRIMARY KEY (CustomerID));
