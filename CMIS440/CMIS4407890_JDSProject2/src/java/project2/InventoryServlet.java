83d2c840e06ab87b88c42e1ec7e9265eba4f6fd0
Justin <phreenet@gmail.com>
2013-04-21 19:18:34 -0400

    Now able to read from silly text file.  WTF that took for ever.

--- CMIS440/CMIS4407890_JDSProject2/src/java/project2/InventoryServlet.java
+++ CMIS440/CMIS4407890_JDSProject2/src/java/project2/InventoryServlet.java
@@ -3,22 +3,27 @@
  *   Created on : Apr 21, 2013, 5:57:50 PM
  *   Author     : Justin Smith
  *   Course     : CMIS 440
- *   Project    : 
+ *   Project    : Project 2
  */
 
 package project2;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- *
+ * Servlet Controller for MVC M2 pattern.
  * @author Justin Smith
  */
 public class InventoryServlet extends HttpServlet {
@@ -32,47 +37,32 @@ public class InventoryServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
+        RequestDispatcher rd;
+        
         try {
             ArrayList<InventoryItem> inventory = new ArrayList<InventoryItem>();
             String inputLine;
             
             System.out.println(new java.io.File("").getAbsolutePath());
             
-            FileReader file = new FileReader("data/ItemsCatalog.txt");
-            BufferedReader reader = new BufferedReader(file);
+            ServletContext con = getServletContext();
+            InputStream in = con.getResourceAsStream("/data/ItemsCatalog.txt");
+            InputStreamReader inr = new InputStreamReader(in);
+            
+            BufferedReader reader = new BufferedReader(inr);
             
             // Read all Inventory Items and add them to the arary list
             while((inputLine = reader.readLine()) != null) {
                 String t[] = inputLine.split(":");
-                InventoryItem item = new InventoryItem(t[1], t[2], t[3], 
-                        Double.parseDouble(t[4]), Integer.parseInt(t[5]));
+                InventoryItem item = new InventoryItem(t[0], t[1], t[2], 
+                        Double.parseDouble(t[3]), Integer.parseInt(t[4]));
                 inventory.add(item);
+                System.out.println(t[0]+":"+t[1]+":"+t[2]+":"+t[3]+":"+t[4]);
             }
         } catch (Exception e) {
             System.err.println(e);
         }
-        
-        
-        
-        
-//        response.setContentType("text/html;charset=UTF-8");
-//        PrintWriter out = response.getWriter();
-//        try {
-//            /* TODO output your page here. You may use following sample code. */
-//            out.println("<!DOCTYPE html>");
-//            out.println("<html>");
-//            out.println("<head>");
-//            out.println("<title>Servlet InventoryServlet</title>");  
-//            out.println("</head>");
-//            out.println("<body>");
-//            out.println("<h1>Servlet InventoryServlet at " + request.getContextPath () + "</h1>");
-//            out.println("</body>");
-//            out.println("</html>");
-//        } finally { 
-//            out.close();
-//        }
     } 
-
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /** 
      * Handles the HTTP <code>GET</code> method.
