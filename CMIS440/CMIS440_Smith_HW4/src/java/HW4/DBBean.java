78d2d5a4f4fb86cc3799bc751d81cc8ebc0c2ea2
Justin <phreenet@gmail.com>
2013-04-27 18:59:58 -0400

    All DBBean core functionality should be in place now.

--- CMIS440/CMIS440_Smith_HW4/src/java/HW4/DBBean.java
+++ CMIS440/CMIS440_Smith_HW4/src/java/HW4/DBBean.java
@@ -59,6 +59,97 @@ public class DBBean {
         return result;
     }
     
+    public void deleteItem(int pk) {
+        Connection c = getConnection();
+        System.out.println("Attempting to delete item: "+pk);
+        try {
+            PreparedStatement ps = c.prepareStatement("DELETE FROM Catalog_T where id=?");
+            ps.setInt(1, pk);
+            ps.execute();
+            ps.close();
+        } catch (Exception e) {
+            System.err.println("Exception thrown from DBBean/deleteItem()");
+            e.printStackTrace();
+        } finally {
+            try {
+                if(!c.isClosed())
+                    c.close();
+            } catch (Exception e) {
+                System.err.println("Exception thrown from DBBean/deleteItem()");
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    public void modifyItem(TableRow tr) {
+        Connection c = getConnection();
+        System.out.println("Attempting to modify item: "+tr.getId());
+        try {
+            PreparedStatement ps = c.prepareStatement("UPDATE Catalog_T SET "
+                    + "Name = ?,"
+                    + "Description = ?,"
+                    + "Price = ?,"
+                    + "QuantityInStock = ? "
+                    + "WHERE ID = ?");
+            
+            ps.setString(1, tr.getName());
+            ps.setString(2, tr.getDesc());
+            ps.setDouble(3, tr.getPrice());
+            ps.setInt(4, tr.getQty());
+            ps.setInt(5, tr.getId());
+            ps.executeUpdate();
+            ps.close();            
+        } catch (Exception e) {
+            System.err.println("Exception thrown from DBBean/modifyItem()");
+            e.printStackTrace();
+        } finally {
+            try {
+                if(!c.isClosed())
+                    c.close();
+            } catch (Exception e) {
+                System.err.println("Exception thrown from DBBean/modifyItem()");
+                e.printStackTrace();
+            }
+            
+        }
+    }
+    
+    public TableRow getItem(int pk) {
+        TableRow row = new TableRow();
+        Connection c = getConnection();
+        ResultSet rset = null;
+        System.out.println("Attempting to get item: "+pk);
+        
+        try {
+            PreparedStatement ps = c.prepareStatement("SELECT * FROM Catalog_T "
+                    + "WHERE ID=?");
+            ps.setInt(1, pk);
+            rset = ps.executeQuery();
+            while(rset.next()) {
+                row.setId(rset.getInt("ID"));
+                row.setName(rset.getString("Name"));
+                row.setDesc(rset.getString("Description"));
+                row.setPrice(rset.getDouble("Price"));
+                row.setQty(rset.getInt("QuantityInStock"));
+            }
+        } catch (Exception e) {
+            System.err.println("Exception thrown from DBBean/getItem()");
+            e.printStackTrace();
+        } finally {
+            try {
+                if(rset != null)
+                    rset.close();                
+                if(!c.isClosed())
+                    c.close();
+            } catch (Exception e) {
+                System.err.println("Exception thrown from DBBean/getItem()");
+                e.printStackTrace();
+            }
+        }
+        
+        return row;
+    }
+    
     private Connection getConnection() {
         Connection c = null;
         try {
