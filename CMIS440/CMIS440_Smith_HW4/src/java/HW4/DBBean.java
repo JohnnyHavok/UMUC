bec88a9f3f49d63ba51857691da30f4d27ac9935
Justin <phreenet@gmail.com>
2013-04-27 16:59:29 -0400

    DBBean now returns an ArrayList instead of a result set to better support MVC 2 pattern.

--- CMIS440/CMIS440_Smith_HW4/src/java/HW4/DBBean.java
+++ CMIS440/CMIS440_Smith_HW4/src/java/HW4/DBBean.java
@@ -11,6 +11,7 @@ package HW4;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import javax.annotation.ManagedBean;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
@@ -23,25 +24,38 @@ import javax.sql.DataSource;
 public class DBBean {
     public DBBean() { }
     
-    public ResultSet getCatalog() {
+    public ArrayList<TableRow> getCatalog() {
         Connection c = getConnection();
         ResultSet rset = null;
+        ArrayList<TableRow> result = new ArrayList<TableRow>();
+        
         try {
             PreparedStatement ps = c.prepareStatement("SELECT * FROM Catalog_T");
             rset = ps.executeQuery();
+            ps.close();
+            while(rset.next()) {
+                TableRow row = new TableRow();
+                row.setId(rset.getInt("ID"));
+                row.setName(rset.getString("Name"));
+                row.setDesc(rset.getString("Description"));
+                row.setPrice(rset.getDouble("Price"));
+                row.setQty(rset.getInt("QuantityInStock"));
+            }            
         } catch (Exception e) {
             System.err.println("Exception thrown from DBBean/getCatalog()");
             System.err.println("e.getMessage()");
         } finally {
             try {
+                if(rset != null)
+                    rset.close();                
                 if(!c.isClosed())
                     c.close();
             } catch (Exception e) {
                 System.err.println("Exception thrown from DBBean/getCatalog()");
                 System.err.println("e.getMessage()");
             }
-        }       
-        return rset;
+        }
+        return result;
     }
     
     private Connection getConnection() {
