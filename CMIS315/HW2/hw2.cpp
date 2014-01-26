8098f0c514cec68fcbf77313e40d4c3c17e178ba
Justin Smith <phreenet@gmail.com>
2014-01-25 20:03:52 -0500

    Finished homework 2

--- CMIS315/HW2/hw2.cpp
+++ CMIS315/HW2/hw2.cpp
@@ -1,9 +1,10 @@
+
 /*	
-**  hw1.cpp
+**  hw2.cpp
 **  Justin Smith
-**  Homework 1 - Rework
+**  Homework 2
 **  CMIS 315.6380
-**  01.18.14
+**  01.25.14
 */
 
 #include <iostream>
@@ -14,6 +15,7 @@ using std::endl;
 
 int main()
 {
+	int count = 0;
 	int input = 0;
 	int sum   = 0;
 	
@@ -21,8 +23,11 @@ int main()
 
 	float average = 0;
 
+	cout << "Please enter the count: ";
+	cin >> count;
+
 	// Loop five times asking for numbers
-	for(int i = 1; i <= 5; i++)
+	for(int i = 1; i <= count; i++)
 	{
 		cout << "Please enter number " << i << ": ";
 		cin >> input;
@@ -35,10 +40,9 @@ int main()
 	}
 
 	// Calculate average
-	average = sum / 5.0;
+	average = static_cast<float>(sum) / count;
 
-	// Output results of the average and product of the five numbers
-	cout << "The average of the five numbers is: " << average << endl;
-	cout << "The product of the five numbers is: " << product << endl;
-
+	// Output results of the average and product of the numbers
+	cout << "The average of the " << count << " numbers is: " << average << endl;
+	cout << "The product of the " << count << " numbers is: " << product << endl;
 }
