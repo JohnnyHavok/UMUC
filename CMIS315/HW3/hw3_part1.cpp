a9d605582a279a7cbbb347647482a20b0bf23125
Justin Smith <phreenet@gmail.com>
2014-02-08 17:48:09 -0500

    Initial hw3 part 1 source

---
+++ CMIS315/HW3/hw3_part1.cpp
@@ -0,0 +1,19 @@
+/*	
+**  hw3_part1.cpp
+**  Justin Smith
+**  Homework 3 Part 1
+**  CMIS 315.6380
+**  02.08.14
+*/
+
+#include <iostream> // cout, endl
+#include <iomanip> // setw
+#include <cstdlib> // srand
+#include <ctime> // time
+
+using namespace std;
+
+int main()
+{
+	const int arraySize = 7 
+}
