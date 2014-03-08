bbb7a3200a93a85dedacdcc840c8ba0ce5bf47af
Justin Smith <phreenet@gmail.com>
2014-03-07 19:25:23 -0500

    Final project code start

---
+++ CMIS315/FP/FinalProject.cpp
@@ -0,0 +1,19 @@
+/*  
+**  FinalProject.cpp
+**  Justin Smith
+**  Final Project
+**  CMIS 315.6380
+**  03.07.14
+*/
+
+#include "Class.h"
+#include "Student.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+}
