182181354776293304691e465c0fe291b484b2af
Justin Smith <phreenet@gmail.com>
2014-03-22 18:05:41 -0400

    Created methods for inserting accounts and transactions.

--- CMIS445/src/DBTools/Bootstrap.java
+++ CMIS445/src/DBTools/Bootstrap.java
@@ -1,6 +1,9 @@
 package DBTools;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Bootstrap {
 
@@ -9,22 +12,7 @@ public class Bootstrap {
     String dbName = "BankDB";
     String URL    = "jdbc:derby:" + dbName + ";create=true";
 
-    Connection conn = null;
-    Statement s;
-    PreparedStatement ps;
-    ResultSet r;
-
-    String createAccountTable =
-        new StringBuilder().append("CREATE TABLE ACCOUNT_T ")
-            .append("( AccountID INT NOT NULL GENERATED ALWAYS AS IDENTITY ")
-            .append("           ( START WITH 50000,  INCREMENT BY 7 ) ")
-            .append("           CONSTRAINT Account_PK PRIMARY KEY, ")
-            .append("  LastName VARCHAR(32) NOT NULL, ")
-            .append("  FirstName VARCHAR(32) NOT NULL, ")
-            .append("  PIN SMALLINT NOT NULL, ")
-            .append("  CheckingBalance DECIMAL(12, 2) NOT NULL, ")
-            .append("  SavingsBalance DECIMAL(12, 2) NOT NULL ) ")
-            .toString();
+    Connection conn;
 
     // -- LOAD JDBC DRIVER --
     try {
@@ -38,16 +26,13 @@ public class Bootstrap {
     // -- CONNECT TO DATABASE --
     try {
       conn = DriverManager.getConnection(URL);
-      s = conn.createStatement();
 
       System.out.println("CONNECTED TO DATABASE "+ dbName);
 
     // -- CHECK FOR TABLES, BUILD TABLES IF NOT PRESENT --
       try {
         if(!DBTools.checkDB(conn)) {
-          System.out.println("CREATING FIRST TIME DATABASE TABLES");
-          System.out.println("CREATING ACCOUNT_T");
-          s.execute(createAccountTable);
+          bootstrap(conn);
         } else {
           System.out.println("DATABASE EXIST, RETAINING DB FOR USE");
         }
@@ -56,7 +41,49 @@ public class Bootstrap {
         System.err.println(e.getMessage());
       }
     } catch (SQLException e) {
-      e.printStackTrace();
+      System.err.println("Unhandled Exception while bootstrapping");
+      System.err.println(e.getMessage());
+    }
+  }
+
+  private static void bootstrap(Connection conn) throws SQLException {
+    Statement s = conn.createStatement();
+
+    // -- Table Creation SQL Statements --
+    String createAccountTable =
+        new StringBuilder().append("CREATE TABLE ACCOUNT_T ")
+            .append("( AccountID INT NOT NULL GENERATED ALWAYS AS IDENTITY ")
+            .append("           ( START WITH 50000,  INCREMENT BY 7 ) ")
+            .append("           CONSTRAINT Account_PK PRIMARY KEY, ")
+            .append("  LastName VARCHAR(32) NOT NULL, ")
+            .append("  FirstName VARCHAR(32) NOT NULL, ")
+            .append("  PIN SMALLINT NOT NULL, ")
+            .append("  CheckingBalance DECIMAL(12, 2) NOT NULL, ")
+            .append("  SavingsBalance DECIMAL(12, 2) NOT NULL ) ")
+            .toString();
+
+    String createTransactionTable =
+        new StringBuilder().append("CREATE TABLE TRANSACTION_T ")
+            .append("( AccountID INT NOT NULL ")
+            .append("       CONSTRAINT AccountID_FK REFERENCES Account_T ")
+            .append("       ON DELETE NO ACTION ON UPDATE RESTRICT, ")
+            .append(" Description VARCHAR(32) NOT NULL, ")
+            .append(" AccountType VARCHAR(8) NOT NULL, ")
+            .append(" Ammount DECIMAL(12, 2) NOT NULL ) ")
+            .toString();
+
+
+
+    try {
+      System.out.println("CREATING FIRST TIME DATABASE TABLES");
+      System.out.println("CREATING ACCOUNT_T");
+      s.execute(createAccountTable);
+      System.out.println("CREATING TRANACTION_T");
+      s.execute(createTransactionTable);
+
+      DBTools.insertAccount(conn, "Smith", "Justin", 1234, 5432.10, 9876.54);
+    } catch (SQLException e) {
+      throw e;
     }
   }
 }
--- CMIS445/src/DBTools/DBTools.java
+++ CMIS445/src/DBTools/DBTools.java
@@ -1,9 +1,64 @@
 package DBTools;
 
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DBTools {
+  protected static int insertAccount( Connection conn,
+                                          String LastName,
+                                          String FirstName,
+                                          int PIN,
+                                          double checkingBal,
+                                          double savingsBal  ) throws SQLException {
+
+    int accountID = 0;
+
+    try {
+      PreparedStatement ps = conn.prepareStatement("INSERT INTO ACCOUNT_T" +
+          "(LastName, FirstName, PIN, CheckingBalance, SavingsBalance) " +
+          "values (?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+      ps.setString(1, LastName);
+      ps.setString(2, FirstName);
+      ps.setInt(3, PIN);
+      ps.setDouble(4, checkingBal);
+      ps.setDouble(5, savingsBal);
+      ps.execute();
+
+      ResultSet rs = ps.getGeneratedKeys();
+
+      if(rs.next()) {
+        accountID = rs.getInt(1);
+        addTransaction(conn, accountID, "New Account Setup", "Checking", checkingBal);
+        addTransaction(conn, accountID, "New Account Setup", "Savings", savingsBal);
+      }
+    } catch (SQLException e) {
+      throw e; // Unhandled SQL Exception
+    }
+
+    return accountID;
+  }
+
+  protected static boolean addTransaction( Connection conn, int accountID, String desc,
+                                           String accType, double ammount ) throws SQLException {
+    try {
+      String transaction = new StringBuilder().append("INSERT INTO TRANSACTION_T")
+          .append(" VALUES(")
+          .append(accountID).append(",'")
+          .append(desc).append("','")
+          .append(accType).append("',")
+          .append(ammount).append(")")
+          .toString();
+
+      Statement s = conn.createStatement();
+      s.execute(transaction);
+
+    } catch (SQLException e) {
+      throw e; // Unhandled SQL Exception
+    }
+    // TODO: Create meaning full test of transaction correctness
+    return true;
+  }
+
+
   protected static boolean checkDB(Connection conn) throws SQLException {
     try {
       conn.createStatement().execute("SELECT * FROM ACCOUNT_T");
