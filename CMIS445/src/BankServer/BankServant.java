2ce2673745c8ce9c7388519d6282763609721ba8
Justin Smith <phreenet@gmail.com>
2014-03-23 10:46:37 -0400

    Simplifying IDL and working with new POA model for implementation.

--- CMIS445/src/BankServer/BankServant.java
+++ CMIS445/src/BankServer/BankServant.java
@@ -1,6 +1,5 @@
 package BankServer;
 
-import BankService.*;
 import DBTools.DBTools;
 import org.omg.CORBA.*;
 import org.omg.CORBA.Object;
@@ -8,7 +7,7 @@ import org.omg.CORBA.Object;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-public class BankServant implements AuthTeller {
+public class BankServant {
   private Connection dbConnection;
 
   public BankServant(Connection c) {
@@ -24,70 +23,5 @@ public class BankServant implements AuthTeller {
       System.err.println(e.getMessage());
     }
     return false;
-  }
-
-  @Override
-  public boolean _is_a(String repositoryIdentifier) {
-    return false;
-  }
-
-  @Override
-  public boolean _is_equivalent(org.omg.CORBA.Object other) {
-    return false;
-  }
-
-  @Override
-  public boolean _non_existent() {
-    return false;
-  }
-
-  @Override
-  public int _hash(int maximum) {
-    return 0;
-  }
-
-  @Override
-  public Object _duplicate() {
-    return null;
-  }
-
-  @Override
-  public void _release() {
-
-  }
-
-  @Override
-  public Object _get_interface_def() {
-    return null;
-  }
-
-  @Override
-  public Request _request(String operation) {
-    return null;
-  }
-
-  @Override
-  public Request _create_request(Context ctx, String operation, NVList arg_list, NamedValue result) {
-    return null;
-  }
-
-  @Override
-  public Request _create_request(Context ctx, String operation, NVList arg_list, NamedValue result, ExceptionList exclist, ContextList ctxlist) {
-    return null;
-  }
-
-  @Override
-  public Policy _get_policy(int policy_type) {
-    return null;
-  }
-
-  @Override
-  public DomainManager[] _get_domain_managers() {
-    return new DomainManager[0];
-  }
-
-  @Override
-  public Object _set_policy_override(Policy[] policies, SetOverrideType set_add) {
-    return null;
   }
 }
--- CMIS445/src/BankServer/BankServer.java
+++ CMIS445/src/BankServer/BankServer.java
@@ -12,33 +12,35 @@ import java.sql.SQLException;
 public class BankServer {
   public static void main(String[] args) {
 
-    try {
-      Connection conn = DBTools.dbConnect();
-      ORB broker = ORB.init(args, null);
 
-      BankServant server = new BankServant(conn);
 
-      broker.connect(server);
-
-      String ref = broker.object_to_string(server);
-      BufferedWriter out = new BufferedWriter(new FileWriter("server.ref"));
-      out.write(ref);
-      out.close();
-
-      java.lang.Object sync = new Object();
-
-      synchronized (sync) { sync.wait(); }
-
-    } catch (SQLException e) {
-      System.err.println("Unhandled SQL Exception reached BankServer.main");
-      System.err.println(e.getMessage());
-    } catch (IOException e) {
-      System.err.println("FileWriter Exception in BankServer.main");
-      System.err.println(e.getMessage());
-    } catch (InterruptedException e) {
-      System.err.println("SYNC Exception in BankServer.main");
-      System.err.println(e.getMessage());
-    }
+//    try {
+//      Connection conn = DBTools.dbConnect();
+//      ORB broker = ORB.init(args, null);
+//
+//      BankServant server = new BankServant(conn);
+//
+//      broker.connect(server);
+//
+//      String ref = broker.object_to_string(server);
+//      BufferedWriter out = new BufferedWriter(new FileWriter("server.ref"));
+//      out.write(ref);
+//      out.close();
+//
+//      java.lang.Object sync = new Object();
+//
+//      synchronized (sync) { sync.wait(); }
+//
+//    } catch (SQLException e) {
+//      System.err.println("Unhandled SQL Exception reached BankServer.main");
+//      System.err.println(e.getMessage());
+//    } catch (IOException e) {
+//      System.err.println("FileWriter Exception in BankServer.main");
+//      System.err.println(e.getMessage());
+//    } catch (InterruptedException e) {
+//      System.err.println("SYNC Exception in BankServer.main");
+//      System.err.println(e.getMessage());
+//    }
 
   }
 }
