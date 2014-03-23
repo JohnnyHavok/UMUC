fcf51c4052bea6192dd66fd3c1cfeb34f222b10c
Justin Smith <phreenet@gmail.com>
2014-03-23 11:14:35 -0400

    Server code finished, using Oracle POA Model example from JDK 7 docs.

--- CMIS445/src/BankServer/BankServant.java
+++ CMIS445/src/BankServer/BankServant.java
@@ -1,17 +1,20 @@
 package BankServer;
 
+import BankService.Customer;
+import BankService.TellerServicePOA;
 import DBTools.DBTools;
-import org.omg.CORBA.*;
-import org.omg.CORBA.Object;
+import org.omg.CORBA.ORB;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 
-public class BankServant {
+public class BankServant extends TellerServicePOA {
   private Connection dbConnection;
+  private ORB broker;
 
-  public BankServant(Connection c) {
-    dbConnection = c;
+  public BankServant(ORB broker, Connection c) {
+    this.broker = broker;
+    this.dbConnection = c;
   }
 
   @Override
@@ -23,5 +26,35 @@ public class BankServant {
       System.err.println(e.getMessage());
     }
     return false;
+  }
+
+  @Override
+  public double deposit(int accountID, String accountType, int amount) {
+    return 0;
+  }
+
+  @Override
+  public double withdraw(int accountID, String accountType, int amount) {
+    return 0;
+  }
+
+  @Override
+  public double cashCheck(int accountID, int checkNumber, int amount) {
+    return 0;
+  }
+
+  @Override
+  public double transfer(int accountID, String accountType, int toAccountID, int amount) {
+    return 0;
+  }
+
+  @Override
+  public int createAccount(Customer newCustomer) {
+    return 0;
+  }
+
+  @Override
+  public void shtudown() {
+    broker.shutdown(true);
   }
 }
--- CMIS445/src/BankServer/BankServer.java
+++ CMIS445/src/BankServer/BankServer.java
@@ -1,46 +1,74 @@
 package BankServer;
 
+import BankService.TellerService;
+import BankService.TellerServiceHelper;
 import DBTools.DBTools;
 import org.omg.CORBA.ORB;
+import org.omg.CORBA.ORBPackage.InvalidName;
+import org.omg.CosNaming.NameComponent;
+import org.omg.CosNaming.NamingContextExt;
+import org.omg.CosNaming.NamingContextExtHelper;
+import org.omg.CosNaming.NamingContextPackage.CannotProceed;
+import org.omg.CosNaming.NamingContextPackage.NotFound;
+import org.omg.PortableServer.POA;
+import org.omg.PortableServer.POAHelper;
+import org.omg.PortableServer.POAManagerPackage.AdapterInactive;
+import org.omg.PortableServer.POAPackage.ServantNotActive;
+import org.omg.PortableServer.POAPackage.WrongPolicy;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public class BankServer {
   public static void main(String[] args) {
+    try {
+      // Create ORB Broker
+      ORB broker = ORB.init(args, null);
 
+      // Get reference to Root POA and activate POAManager
+      POA rootpoa = POAHelper.narrow(broker.resolve_initial_references("RootPOA"));
+      rootpoa.the_POAManager().activate();
 
+      // Create Embedded Database connection
+      Connection dbConnection = DBTools.dbConnect();
 
-//    try {
-//      Connection conn = DBTools.dbConnect();
-//      ORB broker = ORB.init(args, null);
-//
-//      BankServant server = new BankServant(conn);
-//
-//      broker.connect(server);
-//
-//      String ref = broker.object_to_string(server);
-//      BufferedWriter out = new BufferedWriter(new FileWriter("server.ref"));
-//      out.write(ref);
-//      out.close();
-//
-//      java.lang.Object sync = new Object();
-//
-//      synchronized (sync) { sync.wait(); }
-//
-//    } catch (SQLException e) {
-//      System.err.println("Unhandled SQL Exception reached BankServer.main");
-//      System.err.println(e.getMessage());
-//    } catch (IOException e) {
-//      System.err.println("FileWriter Exception in BankServer.main");
-//      System.err.println(e.getMessage());
-//    } catch (InterruptedException e) {
-//      System.err.println("SYNC Exception in BankServer.main");
-//      System.err.println(e.getMessage());
-//    }
+      // Create servant
+      BankServant servant = new BankServant(broker, dbConnection);
 
+      // Get object reference from BankServant
+      org.omg.CORBA.Object ref = rootpoa.servant_to_reference(servant);
+      TellerService href = TellerServiceHelper.narrow(ref);
+
+      // Get the Root Naming Context
+      // Invoke naming service
+      org.omg.CORBA.Object objRef = broker.resolve_initial_references("NameService");
+
+      // Naming Service INS
+      NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef);
+
+      // Bind Object Reference in INS
+      NameComponent path[] = ncRef.to_name("TellerService");
+      ncRef.rebind(path, href);
+
+      System.out.println("BankServer Running....");
+      broker.run();
+
+    } catch (InvalidName invalidName) {
+      invalidName.printStackTrace();
+    } catch (AdapterInactive adapterInactive) {
+      adapterInactive.printStackTrace();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    } catch (WrongPolicy wrongPolicy) {
+      wrongPolicy.printStackTrace();
+    } catch (ServantNotActive servantNotActive) {
+      servantNotActive.printStackTrace();
+    } catch (org.omg.CosNaming.NamingContextPackage.InvalidName invalidName) {
+      invalidName.printStackTrace();
+    } catch (CannotProceed cannotProceed) {
+      cannotProceed.printStackTrace();
+    } catch (NotFound notFound) {
+      notFound.printStackTrace();
+    }
   }
 }
