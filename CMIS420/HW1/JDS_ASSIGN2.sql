67d992831a6694fe9d43cc66620d3faac700968d
Justin Smith <justin.smith5@icloud.com>
2014-03-28 20:36:04 -0400

    Initial Boilerplate Assignment 2

---
+++ CMIS420/HW1/JDS_ASSIGN2.sql
@@ -0,0 +1,38 @@
+/* 
+**  Author: Justin Smith
+**  Course: CMIS 420.7980
+**  Date:  03/28/14
+**  Assignment 2
+*/
+
+/* BEGIN Problem 17 */
+
+/* END Problem 17 */
+
+/* BEGIN Problem 21 */
+
+/* END Problem 21 */
+
+/* BEGIN Problem 22 */
+
+/* END Problem 22 */
+
+/* BEGIN Problem 24 */
+
+/* END Problem 24 */
+
+/* BEGIN Problem 26 */
+
+/* END Problem 26 */
+
+/* BEGIN Problem 27 */
+
+/* END Problem 27 */
+
+/* BEGIN Problem 39 */
+
+/* END Problem 39 */
+
+/* BEGIN Problem 41 */
+
+/* END Problem 41 */
