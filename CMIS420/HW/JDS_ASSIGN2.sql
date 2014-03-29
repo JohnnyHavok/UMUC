44836c5fbad9b15ea29629c8e9dc3b41e10d8cee
Justin Smith <justin.smith5@icloud.com>
2014-03-29 14:28:41 -0400

    Finished up to Problem 24

--- CMIS420/HW/JDS_ASSIGN2.sql
+++ CMIS420/HW/JDS_ASSIGN2.sql
@@ -6,15 +6,36 @@
 */
 
 /* BEGIN Problem 17 */
-
+SELECT E.EmployeeName, E.EmployeeBirthDate, 
+	M.EmployeeName AS Manager, M.EmployeeBirthDate AS ManagerBirth
+  FROM Employee_T E, Employee_T M
+  WHERE E.EmployeeSupervisor = M.EmployeeID
+    AND E.EmployeeBirthDate < M.EmployeeBirthDate;
 /* END Problem 17 */
 
 /* BEGIN Problem 21 */
-
+SELECT P.ProductID, P.ProductDescription, P.ProductStandardPrice,
+ SUM(LineItem) AS TotCost
+FROM Product_T P JOIN 
+  ( SELECT DISTINCT U.ProductID, 
+     (U.QuantityRequired * R.MaterialStandardPrice) AS LineItem
+    FROM Uses_T U, RawMaterial_T R
+    WHERE U.MaterialID = R.MaterialID ) TBL 
+  ON P.ProductID = TBL.ProductID
+GROUP BY P.ProductID, P.ProductDescription, P.ProductStandardPrice
+ORDER BY P.ProductID;
 /* END Problem 21 */
 
 /* BEGIN Problem 22 */
-
+SELECT P.OrderID, (SUM(LineItem)) AS TotalDue, P.PaymentAmount 
+FROM Payment_T P JOIN 
+  ( SELECT DISTINCT OL.OrderID, 
+     (OL.OrderedQuantity * PR.ProductStandardPrice) AS LineItem
+    FROM OrderLine_T OL, Product_T PR
+    WHERE OL.ProductID = PR.ProductID ) ORD 
+  ON P.OrderID = ORD.OrderID
+GROUP BY P.OrderID, P.PaymentAmount
+ORDER BY (TotalDue - P.PaymentAmount) DESC;
 /* END Problem 22 */
 
 /* BEGIN Problem 24 */
