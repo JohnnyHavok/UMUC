f349fedbe773845aee11738e284f83a34adf808b
Justin Smith <justin.smith5@icloud.com>
2014-04-12 19:49:43 -0400

    Problem 3 Complete, Assignment Complete.

--- CMIS420/HW/JDS_ASSIGN3.sql
+++ CMIS420/HW/JDS_ASSIGN3.sql
@@ -29,6 +29,7 @@ BEGIN
 
   CLOSE get_zipcode;
 END;
+/
 -- END NAMED FUNCTION --
 
 -- BEGIN TEST BLOCK --
@@ -54,6 +55,7 @@ BEGIN
     DBMS_OUTPUT.PUT_LINE('Zipcode Passed Does Not Exist');
   END IF;
 END;
+/
 -- END TEST BLOCK --
 -- END PROBLEM 1 --
 
@@ -82,6 +84,7 @@ BEGIN
   CLOSE get_section_count;
 
 END;
+/
 -- END NAMED FUNCTION --
 
 -- BEGIN TEST BLOCK --
@@ -95,15 +98,95 @@ BEGIN
   DBMS_OUTPUT.PUT_LINE('Looking up Instructor: 110');
   DBMS_OUTPUT.PUT_LINE(SECTIONS_COUNT(110));
 END;
+/
 -- END TEST BLOCK --
 -- END PROBLEM 2 --
 
 -- BEGIN PROBLEM 3 --
 -- CREATE STORED PROCEDURE --
+CREATE OR REPLACE PROCEDURE UPDATE_STUDENT(p_studentID IN STUDENT.STUDENT_ID%TYPE,
+                                           p_studentPhone IN STUDENT.PHONE%TYPE,
+                                           p_employer IN STUDENT.EMPLOYER%TYPE)
+IS
+  v_temp        STUDENT.STUDENT_ID%TYPE;
 
+BEGIN
+  -- Check for Student Exist -- 
+  SELECT STUDENT_ID INTO v_temp 
+  FROM STUDENT
+  WHERE STUDENT_ID = p_studentID;
+  -- Will throw NO_DATA_FOUND EXCEPTION --   
+
+  -- If No Exception Thrown -- 
+  UPDATE STUDENT
+  SET PHONE = p_studentPhone,
+      EMPLOYER = p_employer
+  WHERE STUDENT_ID = p_studentID;
+
+  -- If Student Doesn't Exist --
+  -- Catch NO_DATA_FOUND 
+  EXCEPTION
+  WHEN NO_DATA_FOUND THEN
+    INSERT INTO STUDENT
+    (STUDENT_ID, LAST_NAME, ZIP, PHONE, EMPLOYER, REGISTRATION_DATE, 
+      CREATED_BY, CREATED_DATE, MODIFIED_BY, MODIFIED_DATE)
+    VALUES
+    (p_studentID, 'Temp', '10014', p_studentPhone, p_employer, SYSDATE, 
+      'UPDATE_STUDENT', SYSDATE, 'UPDATE_STUDENT', SYSDATE);
+END; 
+/
 -- END STORED PROCEDURE --
 
 -- BEGIN TEST BLOCK --
+DECLARE 
+  v_record        STUDENT%ROWTYPE;
 
+BEGIN 
+  DBMS_OUTPUT.PUT_LINE('Testing Problem 3');
+
+  -- Test Case 1, Update Student 102 --
+  DBMS_OUTPUT.PUT_LINE('Student Record for ID: 102');
+  SELECT * INTO v_record FROM STUDENT WHERE STUDENT_ID = 102;
+  DBMS_OUTPUT.PUT_LINE('Student ID: '|| v_record.STUDENT_ID 
+                        ||' Student Phone: '|| v_record.PHONE 
+                        ||' Student Employer: ' ||v_record.EMPLOYER);
+
+  DBMS_OUTPUT.PUT_LINE('Updating Record for ID: 102');
+  UPDATE_STUDENT(102, '301-744-1982', 'Gregs Electronics');
+
+  DBMS_OUTPUT.PUT_LINE('Student Record for ID: 102');
+  SELECT * INTO v_record FROM STUDENT WHERE STUDENT_ID = 102;
+  DBMS_OUTPUT.PUT_LINE('Student ID: '|| v_record.STUDENT_ID 
+                        ||' Student Phone: '|| v_record.PHONE 
+                        ||' Student Employer: ' ||v_record.EMPLOYER);
+
+  -- Test Case 2, Update Student 103 --
+  DBMS_OUTPUT.PUT_LINE('Student Record for ID: 103');
+  SELECT * INTO v_record FROM STUDENT WHERE STUDENT_ID = 103;
+  DBMS_OUTPUT.PUT_LINE('Student ID: '|| v_record.STUDENT_ID 
+                        ||' Student Phone: '|| v_record.PHONE 
+                        ||' Student Employer: ' ||v_record.EMPLOYER);
+
+  DBMS_OUTPUT.PUT_LINE('Updating Record for ID: 103');
+  UPDATE_STUDENT(103, '757-694-7846', 'McDonalds');
+
+  DBMS_OUTPUT.PUT_LINE('Student Record for ID: 103');
+  SELECT * INTO v_record FROM STUDENT WHERE STUDENT_ID = 103;
+  DBMS_OUTPUT.PUT_LINE('Student ID: '|| v_record.STUDENT_ID 
+                        ||' Student Phone: '|| v_record.PHONE 
+                        ||' Student Employer: ' ||v_record.EMPLOYER);
+
+  -- Test Case 3, Update Student 42, does not exist in my data Set --
+  DBMS_OUTPUT.PUT_LINE('Updating Record for ID: 42');
+  UPDATE_STUDENT(42, '804-412-6958', 'WalMart');
+
+  DBMS_OUTPUT.PUT_LINE('Student Record for ID: 42');
+  SELECT * INTO v_record FROM STUDENT WHERE STUDENT_ID = 42;
+  DBMS_OUTPUT.PUT_LINE('Student ID: '|| v_record.STUDENT_ID 
+                        ||' Student Phone: '|| v_record.PHONE 
+                        ||' Student Employer: ' ||v_record.EMPLOYER);
+
+END;
+/
 -- END TEST BLOCK --
 -- END PROBLEM 3 --
