5ab7f88e2797759b8c5d682062a139ebb23f4d81
Justin Smith <justin.smith5@icloud.com>
2014-05-11 12:21:33 -0400

    Completed Star Schema and Report 1
    Report 1 needs more work grouping by Territory (ZipCode).

--- CMIS420/FP/JDS_FPROJ_REPORTS.sql
+++ CMIS420/FP/JDS_FPROJ_REPORTS.sql
@@ -5,3 +5,13 @@
 **  Final Project
 */
 
+-- BEGIN REPORT 1
+SELECT PY.Type, T.ZipCode AS Territory, TO_CHAR(TO_DATE(PD.Month, 'MM'), 'Month') AS Month, SUM(S.FaceValue)
+  OVER (PARTITION BY S.PolicyCode ORDER BY S.TerritoryCode, S.PeriodCode) AS TotalSales
+FROM Sales S, Policy PY, Territory T, Period PD
+  WHERE S.PolicyCode = PY.PolicyCode 
+    AND S.TerritoryCode = T.TerritoryCode
+    AND S.PeriodCode = PD.PeriodCode
+ORDER BY S.PolicyCode, PD.Month;
+-- END REPORT 1
+
