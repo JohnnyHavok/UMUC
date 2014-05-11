4babb1e63b791bc220840e35cad9ab9c1e20aedb
Justin Smith <justin.smith5@icloud.com>
2014-05-11 12:28:06 -0400

    Completed Report 2.  Needs work grouping zipcodes

--- CMIS420/FP/JDS_FPROJ_REPORTS.sql
+++ CMIS420/FP/JDS_FPROJ_REPORTS.sql
@@ -15,3 +15,13 @@ FROM Sales S, Policy PY, Territory T, Period PD
 ORDER BY S.PolicyCode, PD.Month;
 -- END REPORT 1
 
+-- BEGIN REPORT 2
+SELECT PY.Type, T.ZipCode AS Territory, PD.Quarter, SUM(S.FaceValue)
+  OVER (PARTITION BY PY.PolicyCode ORDER BY PD.Quarter, S.TerritoryCode) AS TotalSales
+FROM Sales S, Policy PY, Territory T, Period PD
+  WHERE S.PolicyCode = PY.PolicyCode 
+    AND S.TerritoryCode = T.TerritoryCode
+    AND S.PeriodCode = PD.PeriodCode
+    AND PD.Year = 2013 -- For previous year's quarters or else they might collide in reporting
+ORDER BY S.PolicyCode, PD.Quarter;
+-- END REPORT 2
