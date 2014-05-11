efa2ba30971ff547b43e93f1d4e4605fe29664d7
Justin Smith <justin.smith5@icloud.com>
2014-05-11 12:44:57 -0400

    Report 1 Fixed, correctly grouped by month and zipcode.

--- CMIS420/FP/JDS_FPROJ_REPORTS.sql
+++ CMIS420/FP/JDS_FPROJ_REPORTS.sql
@@ -6,13 +6,13 @@
 */
 
 -- BEGIN REPORT 1
-SELECT PY.Type, T.ZipCode AS Territory, TO_CHAR(TO_DATE(PD.Month, 'MM'), 'Month') AS Month, SUM(S.FaceValue)
-  OVER (PARTITION BY S.PolicyCode ORDER BY S.TerritoryCode, S.PeriodCode) AS TotalSales
+SELECT PY.Type, T.ZipCode AS Territory, TO_CHAR(TO_DATE(PD.Month, 'MM'), 'Month') AS Month, SUM (S.FaceValue) AS TotalSales
 FROM Sales S, Policy PY, Territory T, Period PD
-  WHERE S.PolicyCode = PY.PolicyCode 
+  WHERE S.PolicyCode = PY.PolicyCode
     AND S.TerritoryCode = T.TerritoryCode
     AND S.PeriodCode = PD.PeriodCode
-ORDER BY S.PolicyCode, PD.Month;
+GROUP BY PY.Type, T.ZipCode, PD.Month
+ORDER BY PY.Type DESC, PD.Month;
 -- END REPORT 1
 
 -- BEGIN REPORT 2
